10c669015e4f8f5b1673e5ebbbab164ca100f112
DASaBeR <m.saberi1999@gmail.com>
2023-02-23 17:33:20 +0100

    change output to 3 pose and type to dictionary(json)

--- Pose_Estimation.py
+++ Pose_Estimation.py
@@ -3,12 +3,13 @@ import cv2
 import mediapipe as mp
 import socket
 import numpy as np
+import json
 
 mpPose = mp.solutions.pose
 pose = mpPose.Pose(static_image_mode=False,
-                                     smooth_landmarks=True,
-                                     min_detection_confidence=0.5,
-                                     min_tracking_confidence=0.5)
+                                     smooth_landmarks=False,
+                                     min_detection_confidence=0.7,
+                                     min_tracking_confidence=0.6)
 mpDraw = mp.solutions.drawing_utils
 fpsReader = cvzone.FPS()
 
@@ -16,7 +17,8 @@ cap = cv2.VideoCapture(0)
 
 ############ Communication ##########
 dataSocket=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-serverAddressPort=("127.0.0.1",5052)
+serverAddressPort=("127.0.0.1",8080)
+
 
 while True:
     success, img = cap.read()
@@ -26,25 +28,35 @@ while True:
 
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = pose.process(imgRGB)
-    data=[]
+    data=([])
     if(results.pose_landmarks):
         land_mark = results.pose_landmarks.landmark
         res_dicts = list(land_mark)
-        #print(type(res_dicts))
-        
-        for i in range(0,len(res_dicts)):
-            #print(len(res_dicts))
-            #print(res_dicts[i])
-            x = res_dicts[i].x
-            y = res_dicts[i].y
-            z = res_dicts[i].z
-            data.append([x , y , z])
-        
+
+        for j in range(0 , len(res_dicts)):
+            if (j == 0 or j == 19 or j == 20):
+                
+                myDic = {
+                    "visibility" : str(res_dicts[j].visibility) ,
+                    "x" : str(res_dicts[j].x),
+                    "y" : str(res_dicts[j].y),
+                    "z" : str(res_dicts[j].z)
+                }
+                myDic2 = {
+                    "pose" : str(j),
+                    "data": str(myDic)
+                }
+                data.append(myDic2)
+            
+        #print(data)
+
      ######## Send data  ##########
         if(data):
-            dataSocket.sendto(str.encode(str(data)),serverAddressPort)
+            data = str.encode(json.dumps(data))
+            ##dataSocket.sendto(str.encode(str(data)),serverAddressPort)
+            dataSocket.sendto(data , serverAddressPort)
             ##### ** Hint ** #####
-            #print(str.encode(str(data)))
+            ##print(str.encode(str(data)))
             #print(data[0][1])
         mpDraw.draw_landmarks(img, results.pose_landmarks,mpPose.POSE_CONNECTIONS)
         
